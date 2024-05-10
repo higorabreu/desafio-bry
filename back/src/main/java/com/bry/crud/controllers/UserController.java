f92182343c12215d2dc91400c698d2d353b01bad
Higor Abreu <higorabreuf@gmail.com>
2024-05-10 16:41:37 -0300

    feat: users migration add

--- back/src/main/java/com/bry/crud/controllers/UserController.java
+++ back/src/main/java/com/bry/crud/controllers/UserController.java
@@ -1,4 +1,22 @@
 package com.bry.crud.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-}
+
+/*   // Endpoint para buscar um usuário pelo ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    } */
+  }
