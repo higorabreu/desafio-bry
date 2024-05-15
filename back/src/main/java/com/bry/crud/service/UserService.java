589fb92339386eac30353b266d671373fa42bdd5
Higor Abreu <higorabreuf@gmail.com>
2024-05-15 01:28:55 -0300

    refactor: remove imports

--- back/src/main/java/com/bry/crud/service/UserService.java
+++ back/src/main/java/com/bry/crud/service/UserService.java
@@ -7,10 +7,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.Page;
 
 import com.bry.crud.domain.user.User;
 import com.bry.crud.domain.user.UserRepository;
